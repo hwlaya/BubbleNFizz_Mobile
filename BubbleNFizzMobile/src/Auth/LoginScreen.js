c6196dfadac24725d9ec2753fec798bb661fd972
hwlaya <main.wayne22@gmail.com>
2024-04-10 21:17:10 +0800

    Updates

--- BubbleNFizzMobile/src/Auth/LoginScreen.js
+++ BubbleNFizzMobile/src/Auth/LoginScreen.js
@@ -17,8 +17,8 @@ import api from "../../config/api";
 const LoginScreen = () => {
   const navigation = useNavigation();
   const user = useContext(UserContext);
-  const [email, setEmail] = useState("email2@gmail.com");
-  const [password, setPassword] = useState("Password1!");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
   const [passwordVisible, setPasswordVisible] = useState(false);
 
